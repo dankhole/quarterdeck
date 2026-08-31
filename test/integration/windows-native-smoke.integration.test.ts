dd8185e5075f39e7fb8af9273cd27f3aaa996a37
d.cole <bogosortmain@gmail.com>
2026-08-31 12:16:50 -0400

    test: avoid Windows ACL module autoload

--- test/integration/windows-native-smoke.integration.test.ts
+++ test/integration/windows-native-smoke.integration.test.ts
@@ -69,7 +69,7 @@ const WINDOWS_ACL_INSPECTION_SCRIPT = [
 	"$ErrorActionPreference = 'Stop'",
 	`$paths = @(ConvertFrom-Json -InputObject ([Environment]::GetEnvironmentVariable('${ACL_INSPECTION_PATHS_KEY}', 'Process')))`,
 	"$currentSid = [System.Security.Principal.WindowsIdentity]::GetCurrent().User.Value",
-	"$rows = @(foreach ($path in $paths) { $acl = Get-Acl -LiteralPath $path; $ownerSid = $acl.GetOwner([System.Security.Principal.SecurityIdentifier]).Value; $rules = @($acl.Access | ForEach-Object { [pscustomobject]@{ sid = $_.IdentityReference.Translate([System.Security.Principal.SecurityIdentifier]).Value; accessType = $_.AccessControlType.ToString(); rights = $_.FileSystemRights.ToString() } }); [pscustomobject]@{ path = $path; currentSid = $currentSid; ownerSid = $ownerSid; protected = $acl.AreAccessRulesProtected; rules = $rules } })",
+	"$rows = @(foreach ($path in $paths) { if ([System.IO.Directory]::Exists($path)) { $acl = [System.IO.Directory]::GetAccessControl($path) } else { $acl = [System.IO.File]::GetAccessControl($path) }; $ownerSid = $acl.GetOwner([System.Security.Principal.SecurityIdentifier]).Value; $rules = @($acl.Access | ForEach-Object { [pscustomobject]@{ sid = $_.IdentityReference.Translate([System.Security.Principal.SecurityIdentifier]).Value; accessType = $_.AccessControlType.ToString(); rights = $_.FileSystemRights.ToString() } }); [pscustomobject]@{ path = $path; currentSid = $currentSid; ownerSid = $ownerSid; protected = $acl.AreAccessRulesProtected; rules = $rules } })",
 	"ConvertTo-Json -InputObject $rows -Depth 5 -Compress",
 ].join("; ");
 
